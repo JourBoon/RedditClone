4b0413a04b014ad7bed149e466867dcfdcdc3f3c
guromain <romain.guibert44@ynov.com>
2026-05-29 09:46:47 +0200

    [ADD] ajout pass_log et mail_log

--- fonctions/get_param.go
+++ fonctions/get_param.go
@@ -6,8 +6,10 @@ import (
 
 type queryParams struct {
 	password	string
+	password_log	string
 	user		string
 	mail		string
+	mail_log	string
 	searchQuery string
 	searchType  string
 	home       	string
@@ -17,8 +19,10 @@ type queryParams struct {
 func extractQueryParams(r *http.Request) queryParams {
 	params := queryParams{
 		password: r.FormValue("Password"),
+		password_log: r.FormValue("Password_log"),
 		user:	r.FormValue("User"),
-		mail:r.FormValue("Mail"),
+		mail:	r.FormValue("Mail"),
+		mail_log:	r.FormValue("Mail_log"),
 		searchQuery: r.FormValue("Search"),
 		searchType:  r.FormValue("SearchType"),
 		home:       r.FormValue("Home"),
