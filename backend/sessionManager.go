14fe86b017662abbe6b557a1bf6015ea4610853f
guromain <romain.guibert44@ynov.com>
2026-06-02 09:00:34 +0200

    [FIX] rajout query manquant

--- backend/sessionManager.go
+++ backend/sessionManager.go
@@ -21,7 +21,7 @@ func generateToken(length int) string {
 func who(r *http.Request) (string,string){
 	var userId string;
 	var userName string;
-	
+	query := `SELECT users.id,users.username FROM users JOIN session ON session.id_user=user.id WHERE session.sessionToken=(?);`
 	db, err := dbConnection()
 	if err != nil {
 		println("erreur dans l'ouverture de la DB")
