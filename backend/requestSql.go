ecc6c97a2cf1ac82d8e184f1d92e787eeb2a4858
guromain <romain.guibert44@ynov.com>
2026-06-12 09:29:10 +0200

    [MODIF] db

--- backend/requestSql.go
+++ backend/requestSql.go
@@ -22,7 +22,7 @@ func insertUser(db *sql.DB, user register) error {
 }
 
 func postMess(db *sql.DB, id_user string, subject string, tag []string, body string) error {
-	query := `INSERT INTO messages (id_user,subject,tag,body) VALUES (?,?,?,?)`
+	query := `INSERT INTO messages (id_user,subject,tags,body) VALUES (?,?,?,?)`
 	println(query, id_user, subject, string(tabStringToJson(tag)), body)
 	_, err := db.Exec(query, id_user, subject, string(tabStringToJson(tag)), body)
 	if err != nil {
@@ -115,7 +115,7 @@ func returnUsername(db *sql.DB, sessionToken string) (string, error) {
 }
 
 func getMess(db *sql.DB) ([]Mess, error) {
-	const postsQuery = `SELECT users.username, messages.subject, messages.body, messages.tag, messages.created_at FROM messages JOIN users ON messages.id_user=users.id`
+	const postsQuery = `SELECT messages.id, users.username, messages.id_user, messages.subject, messages.body, messages.tags, messages.likes, messages.created_at FROM messages JOIN users ON messages.id_user=users.id`
 	rows, err := db.Query(postsQuery)
 	if err != nil {
 		fmt.Println(err)
@@ -125,13 +125,16 @@ func getMess(db *sql.DB) ([]Mess, error) {
 
 	rowsData := []Mess{}
 	for rows.Next() {
+		var id int
 		var username string
+		var userId int
 		var subject string
 		var body string
 		var tagStr string
 		var tag []string
+		var likes int
 		var created_at string
-		err = rows.Scan(&username, &subject, &body, &tagStr, &created_at)
+		err = rows.Scan(&id, &username, &userId, &subject, &body, &tagStr, &likes, &created_at)
 
 		if err != nil {
 			fmt.Println(err.Error())
@@ -141,10 +144,13 @@ func getMess(db *sql.DB) ([]Mess, error) {
 		tag = jsonToTabString([]byte(tagStr))
 
 		rowsData = append(rowsData, Mess{
+			Id:         id,
 			Username:   username,
+			UserId:     userId,
 			Subject:    subject,
 			Body:       body,
 			Tag:        tag,
+			Likes:      likes,
 			Created_at: created_at,
 		})
 	}
