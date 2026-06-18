84422b8745009caf77aab9266bb0d1417cbc78be
guromain <romain.guibert44@ynov.com>
2026-06-18 16:29:00 +0200

    [MODIF] fin

--- backend/hashedPassword.go
+++ backend/hashedPassword.go
@@ -23,11 +23,3 @@ func checkPassword(hashedPassword, passwordLogin string) bool {
     err := bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(passwordLogin))
     return err == nil
 }
-
-func checkHashedPassword(storedPass string, password string) {
-    if checkPassword(storedPass, password) {
-        fmt.Println("The password is correct")
-    } else {
-        fmt.Println("The password is incorrect")
-    }
-}
